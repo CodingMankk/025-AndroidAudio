e4546484b96f7d8788f829ad111319db64ed5706
wsxingjun <1185889308@qq.com>
2018-09-04 21:21:10 +0800

    init

---
+++ app/src/main/java/com/oztaking/www/a025_androidaudio/MainActivity.java
@@ -0,0 +1,18 @@
+package com.oztaking.www.a025_androidaudio;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * @author CodingMankk
+ */
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
